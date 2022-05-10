42184b83dad1647366fa53f9d068345dfa9430e8
Anggun Caksono <25alrescha@gmail.com>
2022-05-10 12:13:03 +0700

    final version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', 'index.html'],
   theme: {
-    extend: {},
+      container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        poppins : ['Poppins']
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
